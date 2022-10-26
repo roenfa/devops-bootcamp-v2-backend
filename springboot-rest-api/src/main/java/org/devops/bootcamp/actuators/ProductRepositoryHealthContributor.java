e038b3a734af88538f1b3eed3356ae9ea1c5cde4
Rocio Encinas Fajardo <rocio.encinas@usalesiana.edu.bo>
2022-10-26 12:00:53 -0400

    Adding health contributor and indicator actuator

---
+++ springboot-rest-api/src/main/java/org/devops/bootcamp/actuators/ProductRepositoryHealthContributor.java
@@ -0,0 +1,35 @@
+package org.devops.bootcamp.actuators;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.CompositeHealthContributor;
+import org.springframework.boot.actuate.health.HealthContributor;
+import org.springframework.boot.actuate.health.NamedContributor;
+import org.springframework.stereotype.Component;
+
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Component("productContributor")
+public class ProductRepositoryHealthContributor implements CompositeHealthContributor {
+
+    private Map<String, HealthContributor> indicators = new LinkedHashMap<>();
+
+    @Autowired
+    public ProductRepositoryHealthContributor(UrlShortenerHealthIndicator urlShorterService)
+    {
+        indicators.put("urlShortener", urlShorterService);
+//        indicators.put("dbService", dbh);
+    }
+
+    @Override
+    public HealthContributor getContributor(String name) {
+        return indicators.get(name);
+    }
+
+    @Override
+    public Iterator<NamedContributor<HealthContributor>> iterator() {
+        return indicators.entrySet().stream().map((entry) ->
+                NamedContributor.of(entry.getKey(), entry.getValue())).iterator();
+    }
+}
