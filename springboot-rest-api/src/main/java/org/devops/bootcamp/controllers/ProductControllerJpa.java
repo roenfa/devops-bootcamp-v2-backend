33511e534366105d5a528a53f8108d78c189ae53
D13g0 <diego.jauregui.salvatierra@gmail.com>
2022-11-05 14:30:49 -0400

    refactor: delete product method

--- springboot-rest-api/src/main/java/org/devops/bootcamp/controllers/ProductControllerJpa.java
+++ springboot-rest-api/src/main/java/org/devops/bootcamp/controllers/ProductControllerJpa.java
@@ -4,6 +4,7 @@ import org.devops.bootcamp.repositories.IProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -66,6 +67,17 @@ public class ProductControllerJpa {
             return new ResponseEntity<>(iProductRepository.save(product), HttpStatus.OK);
         }else{
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Product> deleteProduct(@PathVariable("id") long id){
+        try{
+            iProductRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch(Exception exc){
+            System.out.println(exc.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
