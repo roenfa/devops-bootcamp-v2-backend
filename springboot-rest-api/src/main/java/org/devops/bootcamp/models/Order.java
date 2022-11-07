59341519ad8b49764ba31119fa0f7fb719130b00
Martin <martinferreyrae@hotmail.com>
2022-11-07 12:08:30 -0300

    feat: Updated final project

--- springboot-rest-api/src/main/java/org/devops/bootcamp/models/Order.java
+++ springboot-rest-api/src/main/java/org/devops/bootcamp/models/Order.java
@@ -1,17 +1,36 @@
 package org.devops.bootcamp.models;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
+import javax.persistence.*;
 import java.util.List;
 
-@Getter
-@Setter
+@Data
+@Entity
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table( name = "product_order")
 public class Order {
-    private int orderId;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer orderId;
+
+    @Getter(AccessLevel.NONE)
     private double total;
+
     private String client;
+
+    @ManyToMany(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
     private List<Product> productList;
+
+    public double getTotal() {
+        return this.productList
+                .stream()
+                .reduce(
+                        0.0, (acc, product) -> acc + product.getPrice(), (t, u) -> Double.sum(t, u)
+                );
+    }
+
 }
