9a37cc1f15dc42c666f1c444ce7a2f13ac922b1c
Andres Galvis B <a.galvisbolivar@gmail.com>
2022-11-07 10:10:20 -0500

    feat: contact and employee controllers

--- springboot-mvc/src/main/java/org/devops/bootcamp/controllers/ContactController.java
+++ springboot-mvc/src/main/java/org/devops/bootcamp/controllers/ContactController.java
@@ -1,13 +1,13 @@
 package org.devops.bootcamp.controllers;
 
 import org.devops.bootcamp.models.ContactEntry;
+import org.devops.bootcamp.models.Employee;
 import org.devops.bootcamp.services.ContactService;
+import org.devops.bootcamp.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -27,10 +27,16 @@ public class ContactController {
         return mv;
     }
 
-    @RequestMapping("/create-contact")
+    @RequestMapping(value="/create-contact")
     public String showCreateContactPage(Model model) {
         model.addAttribute("command", new ContactEntry());
         return "create-contact";
+    }
+    @RequestMapping(value = "/update-contact/{id}")
+    public String showUpdateContactPage(@PathVariable("id") int id, Model model) {
+        ContactEntry contact = contactService.findById(id).orElse(null);
+        model.addAttribute("command", contact);
+        return "update-contact";
     }
 
     @RequestMapping(value = "/create-contact", method = RequestMethod.POST)
@@ -38,4 +44,17 @@ public class ContactController {
         contactService.saveContact(contact);
         return "redirect:/list-contacts";
     }
+
+    @RequestMapping(value = "/update-contact", method = RequestMethod.POST)
+    public String updateContact(@ModelAttribute("contact") ContactEntry contact) {
+        contactService.updateContact(contact.getId(), contact);
+        return "redirect:/list-contacts";
+    }
+
+    @PostMapping("/delete-contact/{id}")
+    public String deleteContact(@PathVariable("id") Integer id) {
+        contactService.deleteById(id);
+        return "redirect:/list-contacts";
+    }
+
 }
--- springboot-mvc/src/main/java/org/devops/bootcamp/controllers/EmployeeController.java
+++ springboot-mvc/src/main/java/org/devops/bootcamp/controllers/EmployeeController.java
@@ -1,44 +1,61 @@
-package org.devops.bootcamp.controllers;
+package org.devops.bootcamp.controller;
 
 
-import org.devops.bootcamp.constants.ConstantVariable;
 import org.devops.bootcamp.models.Employee;
-import org.devops.bootcamp.repositories.EmployeeRepository;
+import org.devops.bootcamp.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.Model;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/employee")
 public class EmployeeController {
 
     @Autowired
-    private EmployeeRepository employeeRepository;
+    private EmployeeService employeeService;
 
-    /* List added employees. */
-    @RequestMapping(value="/listEmployee", method= RequestMethod.GET)
-    public String listEmployeeList(Model model) {
-        List<Employee> employeeList = this.employeeRepository.findAll();
-
-        model.addAttribute(ConstantVariable.EMPLOYEE_LIST, employeeList);
-
-        return "listEmployee";
+    @RequestMapping("/list-employees")
+    public ModelAndView getAllEmployees() {
+        ModelAndView mv= new ModelAndView();
+        List<Employee> employees = employeeService.findAll();
+        mv.addObject("employees", employees);
+        mv.setViewName("list-employees");
+        return mv;
     }
 
-    /* Display add employee form page. */
-    @RequestMapping(value="/addEmployeePage", method=RequestMethod.GET)
-    public String addEmployeePage() {
-        return "addEmployee";
+    @RequestMapping("/create-employee")
+    public String createEmployeePage(Model model) {
+        model.addAttribute("command", new Employee());
+        return "create-employee";
     }
 
     /* Add user submitted employee info to h2 databae and redirect to list employees page. */
-    @RequestMapping(value="/addEmployee", method=RequestMethod.POST)
-    public String addEmployee(Employee employee) {
-        employeeRepository.save(employee);
-        return "redirect:/employee/listEmployee";
+    @RequestMapping(value="/create-employee", method=RequestMethod.POST)
+    public String createEmployee(Employee employee) {
+        this.employeeService.saveEmployee(employee);
+        return "redirect:/list-employees";
+    }
+
+    @RequestMapping("/update-employee/{id}")
+    public String updateEmployeePage(@PathVariable("id") int id, Model model) {
+        model.addAttribute("command", employeeService.findById(id).orElse(null));
+        return "update-employee";
+    }
+
+    @RequestMapping(value="/update-employee", method=RequestMethod.POST)
+    public String updateEmployee(Employee employee) {
+        this.employeeService.updateEmployee(employee.getId(), employee);
+        return "redirect:/list-employees";
+    }
+
+    @RequestMapping(value="/delete-employee/{id}", method=RequestMethod.POST)
+    public String deleteEmployee(@PathVariable("id") int id) {
+        this.employeeService.deleteEmployee(id);
+        return "redirect:/list-employees";
     }
 }
