4dc758573c8bb42cbe21c0d3c8e2dea4c368f385
D13g0 <diego.jauregui.salvatierra@gmail.com>
2022-11-10 14:46:44 -0400

    style, fix: removing query annotations and comments con CrudRepository (Not needed anymore)

--- springboot-rest-api/src/main/java/org/devops/bootcamp/repositories/IUserRepository.java
+++ springboot-rest-api/src/main/java/org/devops/bootcamp/repositories/IUserRepository.java
@@ -2,13 +2,10 @@ package org.devops.bootcamp.repositories;
 
 import org.devops.bootcamp.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-// import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IUserRepository extends JpaRepository<User, Long> {
-    @Query("SELECT u FROM User u WHERE u.username = :username")
     public User getUserByUsername(@Param("username") String username);
 }
